8eebf1d586c47bf552d0908731f574abbea743c0
genosDev <snsarij@amazon.com>
2021-04-26 12:42:52 +0530

    added env

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
+console.log(process.env.NAME);
+
 class App extends Component {
   render() {
     return (
